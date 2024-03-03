b92c0953275214deef6d7494da96e36b4eb7992c
Preston Lam <preslam911@gmail.com>
2024-03-02 23:33:21 -0700

    added css

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const body = document.querySelector('body');
-const display = document.createElement('input');
-const dotBtn = document.createElement('button');
-const dot = document.createTextNode('.')
-let count = 0;
+// const body = document.querySelector('body');
+// const display = document.createElement('input');
+// const dotBtn = document.createElement('button');
+// const dot = document.createTextNode('.')
+// let count = 0;
 
-display.setAttribute('style', 'border: 3px solid purple; width: 200px; height: 50px; font-size: 30px;');
-dotBtn.appendChild(dot);
-dotBtn.setAttribute('type', 'button');
-dotBtn.setAttribute('style', 'display: block; width: 50px; height: 50px; font-size: 30px;');
-body.appendChild(display);
-body.appendChild(dotBtn);
+// display.setAttribute('style', 'border: 3px solid purple; width: 200px; height: 50px; font-size: 30px;');
+// dotBtn.appendChild(dot);
+// dotBtn.setAttribute('type', 'button');
+// dotBtn.setAttribute('style', 'display: block; width: 50px; height: 50px; font-size: 30px;');
+// body.appendChild(display);
+// body.appendChild(dotBtn);
 
-dotBtn.addEventListener('click', event => {
-    if (!display.value.includes('.')) {
-        display.value += event.target.textContent;
-    };
-});
+// dotBtn.addEventListener('click', event => {
+//     if (!display.value.includes('.')) {
+//         display.value += event.target.textContent;
+//     };
+// });
 
-[display, window].forEach(item => {
-    item.addEventListener('keypress', event => {
-        event.stopImmedidateProgagation;
-        if (event.key === '.') {
-            if (!display.value.includes('.')) {
-                display.value += event.key;
-            };
-        };
+// [display, window].forEach(item => {
+//     item.addEventListener('keypress', event => {
+//         event.stopImmedidateProgagation;
+//         if (event.key === '.') {
+//             if (!display.value.includes('.')) {
+//                 display.value += event.key;
+//             };
+//         };
         
-    });
-});
+//     });
+// });
 
 
