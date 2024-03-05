e33a22441b8d85ebf53780e474c8bb11311c39c1
Preston Lam <preslam911@gmail.com>
2024-03-05 09:17:13 -0700

    edited buttons

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const mainScreen = document.getElementById('main-screen');
 const subScreen = document.getElementById('sub-screen');
 
 const buttons = document.querySelector('.button-wrapper');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('backspace');
 
 function isNumber(node) {
     const regex = new RegExp('^\\d$', 'g')
@@ -13,7 +15,7 @@ const numberButtons = [...buttons.children].filter(isNumber);
 numberButtons.forEach(button => 
     button.addEventListener('click', event => {
         if (/^0$/g.test(mainScreen.textContent)) {
-            mainScreen.textContent = '';
+            mainScreen.textContent = 0;
         }
         if (mainScreen.textContent.length < 10) {
             mainScreen.textContent += event.target.textContent;
@@ -23,10 +25,17 @@ numberButtons.forEach(button =>
 
 window.addEventListener('keydown', event => {
     if (/^0$/g.test(mainScreen.textContent)) {
-        mainScreen.textContent = '';
+        mainScreen.textContent = 0;
     }
     if (/^\d$/g.test(event.key) && mainScreen.textContent.length < 10) {
         mainScreen.textContent += event.key;
     }
 })
 
+clearBtn.addEventListener('click', () => {
+    mainScreen.textContent = 0;
+})
+
+deleteBtn.addEventListener('click', () => {
+    mainScreen.textContent = mainScreen.textContent.slice(0, -1) || 0;
+})
