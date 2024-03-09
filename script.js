5c7ec995f25ba235fb68e66a9b03dad964cb368f
Preston Lam <preslam911@gmail.com>
2024-03-09 14:53:04 -0700

    changed the classes for buttons

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const mainScreen = document.getElementById('main-screen');
 const subScreen = document.getElementById('sub-screen');
 
-const buttons = document.querySelector('.button-wrapper');
+
+const numberButtons = document.getElementsByClassName('digits');
+const operatorButtons = document.getElementsByClassName('operators');
+
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('backspace');
 
-function isNumber(node) {
-    const regex = new RegExp('^\\d$', 'g')
-    return regex.test(node.textContent);
-}
-
-const numberButtons = [...buttons.children].filter(isNumber);
-
-numberButtons.forEach(button => 
+[...numberButtons].forEach(button => 
     button.addEventListener('click', event => {
         if (/^0$/g.test(mainScreen.textContent)) {
-            mainScreen.textContent = 0;
+            mainScreen.textContent = '';
         }
         if (mainScreen.textContent.length < 10) {
             mainScreen.textContent += event.target.textContent;
@@ -23,19 +19,26 @@ numberButtons.forEach(button =>
     })
 );
 
-window.addEventListener('keydown', event => {
-    if (/^0$/g.test(mainScreen.textContent)) {
-        mainScreen.textContent = 0;
-    }
-    if (/^\d$/g.test(event.key) && mainScreen.textContent.length < 10) {
-        mainScreen.textContent += event.key;
-    }
-})
+[...operatorButtons].forEach(button => 
+    button.addEventListener('click', event => {
+        console.log(event.target.textContent);
+    })    
+)
 
-clearBtn.addEventListener('click', () => {
-    mainScreen.textContent = 0;
-})
+// window.addEventListener('keydown', event => {
+//     if (/^0$/g.test(mainScreen.textContent) && /^\d$/g.test(event.key)) {
+//         mainScreen.textContent = '';
+//     }
+//     if (/^\d$/g.test(event.key) && mainScreen.textContent.length < 10) {
+//         mainScreen.textContent += event.key;
+//     }
 
-deleteBtn.addEventListener('click', () => {
-    mainScreen.textContent = mainScreen.textContent.slice(0, -1) || 0;
-})
+// })
+
+// clearBtn.addEventListener('click', () => {
+//     mainScreen.textContent = 0;
+// })
+
+// deleteBtn.addEventListener('click', () => {
+//     mainScreen.textContent = mainScreen.textContent.slice(0, -1) || 0;
+// })
